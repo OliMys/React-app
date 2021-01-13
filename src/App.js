65ba50af1ed14467b8438221f1a27a8ff8ab9336
Olga <67907193+OliMys@users.noreply.github.com>
2021-01-13 23:58:17 +0300

    Update App.js
    Добавление компонентов сортировки и пагинации

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
-
 import React, {Component} from 'react';
+import Pagination from './components/Pagination';
 import ActiveUser from './components/ActiveUser';
-import UserList from './components/UserList'
-import SearchBar from './components/SearchBar'
+import UserList from './components/UserList';
+import SearchBar from './components/SearchBar';
+//import AddUser from './components/AddUser';
+import './index.css'
+//import DataSort from './components/DataSort';
+
+
 
 export default class App extends Component {
     constructor(props) {
@@ -11,13 +16,16 @@ export default class App extends Component {
             data: [],
             filterText: '',
             isLoaded: false,
-            active: null
-            
+            active: null,
+            currentPage: 1,
+            rowsPerPage: 10,    
         };
         //this.componentDidMount();
         this.updateData = this.updateData.bind(this);
+       
+        
     }
-    
+   
     componentDidMount() {
         fetch(this.props.url)
         .then(response => response.json()) 
@@ -34,20 +42,27 @@ export default class App extends Component {
     updateData(config) {
         this.setState(config);
       } 
-         
+    
+    getCurrentRows() {
+        const indexOfLastRow = this.state.currentPage * this.state.rowsPerPage;
+        const indexOfFirstRow = indexOfLastRow - this.state.rowsPerPage; 
+        const currentRows = this.state.data.slice(indexOfFirstRow, indexOfLastRow);
+        return currentRows;
+    }
     
     render() {
         const {isLoaded} = this.state;
         if (!isLoaded) { return (<p>Loading...</p>); }
         return (
             <div className="container">
-                <SearchBar data={this.initilData} filterText={this.state.filterText} updateData={this.updateData}/>
-                <UserList data={this.state.data} updateData={this.updateData}/>
+                <div><SearchBar data={this.initilData} filterText={this.state.filterText} updateData={this.updateData}/></div>
+                
+                <UserList data={this.getCurrentRows()} updateData={this.updateData} totalData={this.initilData}/>
                 <div className="active-user-block">
                     <ActiveUser data={this.state.data} active={this.state.active} />
                 </div>
+                <Pagination rowsPerPage={this.state.rowsPerPage} data={this.state.data} updateData={this.updateData} />
             </div>
-            
             
         )}
     }
