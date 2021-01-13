dbcd45f277fce6a2bc774af77a3100998e276b53
Olga <67907193+OliMys@users.noreply.github.com>
2021-01-13 23:55:41 +0300

    Update UserList.js

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,23 +1,21 @@
 import React from 'react';
+import DataSort from './DataSort'
 import UserData from './UserData';
 
 export default function UserList (props) {
     //if (!data) { return (<p>Loading...</p>); }
     const data = props.data;
+    const totalData = props.totalData;
     const updateData = props.updateData;
     const users = data.map((user, index) => {
         return (<UserData user = {user}  index={index} key={`user-${index}`} updateData={updateData}/>)
     });
+    const headCaption = [{label: 'id'}, {label: 'firstName'}, {label: 'lastName'}, {label: 'email'}, {label: 'phone'}];
+    const tableHead = headCaption.map((elem, index) => <th key={index}><DataSort data={totalData} updateData={updateData} type={elem.label} label={elem.label}/></th>);
     return (
-        <table className="container__table">
+        <table className="container-table table">
             <thead>
-                <tr >
-                    <th>Id</th>
-                    <th>First Name</th>
-                    <th>Last Name</th>
-                    <th>Email</th>
-                    <th>Phone</th>
-                </tr>
+                <tr className="table-caption">{tableHead}</tr>
             </thead>
             <tbody>
                 {users}
